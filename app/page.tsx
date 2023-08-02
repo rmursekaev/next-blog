cbdd40420dbd29a20aabd17442d36b532b774316
Rashid Mursekaev <r.mursekaev@gmail.com>
2023-08-02 15:16:48 +0100

    initial set up complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <main className={inter.className}>
-      
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Rashid</span>.
+        </span>
+      </p>
     </main>
   )
 }
